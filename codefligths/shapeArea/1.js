2d760e36aae3795c740b02e7fd109661a569e124
DiegoDulval <diegodifreitas@gmail.com>
2018-04-18 17:07:49 -0300

    Update areaShape problem

--- codefligths/shapeArea/1.js
+++ codefligths/shapeArea/1.js
@@ -5,12 +5,5 @@ console.log(shapeArea(3));
 console.log(shapeArea(4));
 
 function shapeArea(n) {
-  if (n == 1) {
-    return 1;
-  }
-  let soma = 0;
-  for (let i = 0; i < n; i++) {
-    soma = i * n;
-  }
-  return soma - (n - 1) + n * n;
+  return Math.pow((n - 1),2) + Math.pow(n, 2);
 }
